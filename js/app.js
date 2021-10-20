bb4799790443db70a36dd1fd2808d93a928ec52c
mohamadsaad1 <mohamad_utd@outlook.com>
2021-10-20 15:02:11 -0500

    fixed spacing and phrases

--- js/app.js
+++ js/app.js
@@ -94,20 +94,19 @@ function checkDie(){
   loseBool = false
   if (players[turn][0]>=1 && players[turn][0]<=8){
       if (tiles[(players[turn][0]%10)-1]==0){
-        statusEl.innerText="player "+(turn+1).toString()+" lose"
+        statusEl.innerText="player "+(turn+1).toString()+" lost"
         players[turn][1]=false
         loseBool=checkLose()
       }
   }
   else if(players[turn][0]>=21 && players[turn][0]<=28){
       if (tiles[(players[turn][0]%10)-1]==1){
-          statusEl.innerText="player "+(turn+1).toString()+" lose"
+          statusEl.innerText="player "+(turn+1).toString()+" lost"
           players[turn][1]=false
           loseBool=checkLose()
       }
   }
   return loseBool
-
 }
 
 function turns(){
@@ -121,66 +120,53 @@ function turns(){
       turn=2
   } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==false){
       turn=0
-  }
-  else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==true){
+  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==true){
       turn=1
-  }
-  else if (turn==1 && players[0][1]==false && players[2][1]==false){
+  } else if (turn==1 && players[0][1]==false && players[2][1]==false){
       turn=1
+  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==false){
+    turn=1
+  } else if (turn==1 && players[2][1]==true){
+    turn=2
+  } else if (turn==1 && players[2][1]==false && players[0][1]==true){
+    turn=0
+  } else if (turn==2 && players[0][1]==true && players[0][2]==false){
+    turn=0
+  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==false){
+    turn=1
+  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==true){
+    turn=2
+  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==false){
+    turn=2
+  } else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==false){
+    turn=1
+  } else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==true){
+    turn=2
+  } else if (turn==2 && players[0][1]==false && players[1][1]==false){
+    turn=2
+  } else {
+    turn=-1
   }
-  else if (turn==1 && players[turn][0]%10==0 && players[0][1]==false){
-      turn=1
-  }
-  else if (turn==1 && players[2][1]==true){
-      turn=2
-  }
-  else if (turn==1 && players[2][1]==false && players[0][1]==true){
-      turn=0
-  }
-  else if (turn==2 && players[0][1]==true && players[0][2]==false){
-      turn=0
-  }
-  else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==false) {
-      turn=1
-  }
-  else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==true){
-      turn=2
-  }
-  else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==false){
-      turn=2
-  }
-  else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==false){
-      turn=1
-  }
-  else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==true){
-      turn=2
-  }
-  else if (turn==2 && players[0][1]==false && players[1][1]==false){
-      turn=2
-  }
-  else{
-      turn=-1
-  }
-  turnHtml.innerText="player's "+(turn+1).toString()+"turn to play";
+  turnHtml.innerText="player "+(turn+1).toString()+"'s turn to play";
 }
 
 function checkHold(turn){
   if (players[turn][0]%10 == ((players[turn-1][0]%10)-1)){
-      return true;
+    return true;
   }
   else{
-      return false;
+    return false;
   }
 }
 
 function checkPass(){
   winBool=false
-    if (players[turn][0]%10==9){
-      squares[players[turn][0]].classList.remove('player'+(turn+1).toString());
-      statusEl.innerText="player "+(turn+1).toString()+" win";
-      players[turn][2]=true;
-      winBool = checkWin();
-    }
+  if (players[turn][0]%10==9){
+    squares[players[turn][0]].classList.remove('player'+(turn+1).toString());
+    statusEl.innerText="player "+(turn+1).toString()+" win";
+    layers[turn][2]=true;
+    winBool = checkWin();
+  }
   return winBool
 }
 
@@ -190,57 +176,56 @@ function hideButtons(){
   holdBtn.style.visibility = 'hidden'
   pushBtn.style.visibility = 'hidden'
 }
-  
+
 function showButtons(){
-    if (turn==0){
+  if (turn==0){
     moveUpBtn.style.visibility = 'visible'
     moveDownBtn.style.visibility = 'visible'
     holdBtn.style.visibility = 'visible'
-    }
-    else if (turn == 1 && players[0][1]==true){
-        if (checkHold(turn)==true){
-            holdBtn.style.visibility = 'visible'
-            pushBtn.style.visibility = 'visible'
-        } else {
-            moveUpBtn.style.visibility = 'visible'
-            moveDownBtn.style.visibility = 'visible'
-            holdBtn.style.visibility = 'visible'
-        }
-    } else if (turn == 1 && players[0][1]==false){
-        moveUpBtn.style.visibility = 'visible'
-        moveDownBtn.style.visibility = 'visible'
+  } else if (turn == 1 && players[0][1]==true){
+      if (checkHold(turn)==true){
         holdBtn.style.visibility = 'visible'
-    } else if (turn == 2 && players[1][1]==true && players[0][1]==true){
-        if (checkHold(turn) && checkHold(turn-1)){
-          holdBtn.style.visibility = 'visible'
-        } else if (checkHold(turn)){
-          holdBtn.style.visibility = 'visible'
-          pushBtn.style.visibility = 'visible'
-        } else {
+        pushBtn.style.visibility = 'visible'
+      } else {
           moveUpBtn.style.visibility = 'visible'
           moveDownBtn.style.visibility = 'visible'
           holdBtn.style.visibility = 'visible'
         }
-    } else if (turn == 2 && players[1][1]==true && players[0][1]==false){
-    if (checkHold(turn)==true){
+  } else if (turn == 1 && players[0][1]==false){
+      moveUpBtn.style.visibility = 'visible'
+      moveDownBtn.style.visibility = 'visible'
+      holdBtn.style.visibility = 'visible'
+  } else if (turn == 2 && players[1][1]==true && players[0][1]==true){
+      if (checkHold(turn) && checkHold(turn-1)){
         holdBtn.style.visibility = 'visible'
-        pushBtn.style.visibility = 'visible'
+      } else if (checkHold(turn)){
+          holdBtn.style.visibility = 'visible'
+          pushBtn.style.visibility = 'visible'
+      } else {
+          moveUpBtn.style.visibility = 'visible'
+          moveDownBtn.style.visibility = 'visible'
+          holdBtn.style.visibility = 'visible'
+      }
+  } else if (turn == 2 && players[1][1]==true && players[0][1]==false){
+    if (checkHold(turn)==true){
+      holdBtn.style.visibility = 'visible'
+      pushBtn.style.visibility = 'visible'
     } else {
         moveUpBtn.style.visibility = 'visible'
         moveDownBtn.style.visibility = 'visible'
         holdBtn.style.visibility = 'visible'
     }
-    } else if (turn == 2 && players[1][1]==false){ 
-        moveUpBtn.style.visibility = 'visible'
-        moveDownBtn.style.visibility = 'visible'
-        holdBtn.style.visibility = 'visible'
-    }
+  } else if (turn == 2 && players[1][1]==false){ 
+      moveUpBtn.style.visibility = 'visible'
+      moveDownBtn.style.visibility = 'visible'
+      holdBtn.style.visibility = 'visible'
+  }
 }
 
 function moveUp(){
   squares[players[turn][0]].classList.remove('player'+(turn+1).toString())
   if (players[turn][0]>=1 && players[turn][0]<=8){
-          players[turn][0]=players[turn][0]+1;
+        players[turn][0]=players[turn][0]+1;
       } else if(players[turn][0]>=21 && players[turn][0]<=28){
           players[turn][0]=players[turn][0] - 19;
       } else if(players[turn][0]%10 == 0) {
@@ -258,19 +243,15 @@ function moveUp(){
   }
 }
 
-function moveDown()
-  {
+function moveDown(){
     squares[players[turn][0]].classList.remove('player'+(turn+1).toString())
     if (players[turn][0]>=1 && players[turn][0]<=8){
-            players[turn][0]=players[turn][0] + 21;
-        }
-    else if(players[turn][0]>=21 && players[turn][0]<=28){
+          players[turn][0]=players[turn][0] + 21;
+        } else if(players[turn][0]>=21 && players[turn][0]<=28){
             players[turn][0]=players[turn][0] + 1;
-        }
-    else if(players[turn][0]%10 == 0)
-    {
+        } else if(players[turn][0]%10 == 0){
         players[turn][0] = 21
-    }
+        }
     squares[players[turn][0]].classList.add('player'+(turn+1).toString())
     hideButtons()
     lose = checkDie()
@@ -279,8 +260,7 @@ function moveDown()
     if (!win && !lose){
         showButtons()
     }
-
-  }
+}
 
 function hold(){
   hideButtons();
@@ -302,10 +282,10 @@ function push(){
     win = checkPass();
     turn=turn+1;
     turns();
-  if (!win && !lose){
-      showButtons();
-  } 
-}else {
+    if (!win && !lose){
+    showButtons();
+    } 
+  } else {
       squares[players[2][0]].classList.remove('player'+(3).toString())
       squares[players[1][0]].classList.remove('player'+(2).toString())
       players[2][0]=players[1][0]
@@ -319,8 +299,8 @@ function push(){
       turn=turn+1;
       turns();
       if (!win && !lose){
-          showButtons();
+        showButtons();
       }
-    }
   }
+}
 
