041caa81d8a826adad14e14a1c41ece2ee827bc4
mohamadsaad1 <mohamad_utd@outlook.com>
2021-10-21 13:08:14 -0500

    add character to player 2

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ players = []
 for (let i = 0; i < 3; i++) {
   players.push([postion=0+i*10,true,false])
 }
-
+// second index(true)in player refers to alive status
+//third index(false) refers to passing last tile 
 console.log(tiles)
 /*--------- Cached Element References ---------*/
 
@@ -42,17 +43,6 @@ startBtn.addEventListener('click', start)
 lightDarkBtn.addEventListener("click", toggleLightDark)
 
 /*----------------- Functions -----------------*/
-
-function play() {
-  currentTime--
-  timeLeft.innerText = currentTime
-  if (currentTime==0){
-      clearInterval(timer);
-      statusEl.innerText="you lose"
-      hideButtons()
-  }
-}
-
 function start() {
   if(timer) {
   } else {
@@ -64,119 +54,15 @@ function start() {
     }
   }
 }
-function checkLose(){
-  for(let i = 0; i < 3; i++) {
-    if (players[i][1]==true){
-      return false
-    }
-  }
-  statusEl.innerText="you lose"
-  clearInterval(timer);
-  hideButtons()
-  return true
-}
 
-function checkWin(){
-  win = false
-  for(let i = 0; i < 3; i++) {
-    if (players[i][2]==false && players[i][1]==true){
-      win = true
-    }
-  } 
-  if (win){
-    statusEl.innerText="you win"
-    clearInterval(timer);
-    hideButtons()
+function play() {
+  currentTime--
+  timeLeft.innerText = currentTime
+  if (currentTime==0){
+      clearInterval(timer);
+      statusEl.innerText="you lose"
+      hideButtons()
   }
-  return win
-}
-
-function checkDie(){
-  loseBool = false
-  if (players[turn][0]>=1 && players[turn][0]<=8){
-    if (tiles[(players[turn][0]%10)-1]==0){
-      statusEl.innerText="player "+(turn+1).toString()+" lost"
-      players[turn][1]=false
-      
-      loseBool=checkLose()
-    }
-  }
-  else if(players[turn][0]>=21 && players[turn][0]<=28){
-    if (tiles[(players[turn][0]%10)-1]==1){
-      statusEl.innerText="player "+(turn+1).toString()+" lost"
-      players[turn][1]=false
-      loseBool=checkLose()
-    }
-  }
-  return loseBool
-}
-
-function turns(){
-  if (turn==0 && players[0][0]%10==0){
-    turn=0
-  } else if (turn==0 && players[1][1]==true){
-    turn=1
-  } else if (turn==0 && players[1][1]==false && players[2][1]==false){
-    turn=0
-  } else if (turn==0 && players[1][1]==false && players[2][1]==true){
-    turn=2
-  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==false){
-    turn=0
-  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==true){
-    turn=1
-  } else if (turn==1 && players[0][1]==false && players[2][1]==false){
-    turn=1
-  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==false){
-    turn=1
-  } else if (turn==1 && players[2][1]==true){
-    turn=2
-  } else if (turn==1 && players[2][1]==false && players[0][1]==true){
-    turn=0
-  } else if (turn==2 && players[0][1]==true && players[0][2]==false){
-    turn=0
-  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==false){
-    turn=1
-  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==true){
-    turn=2
-  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==false){
-    turn=2
-  } else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==false){
-    turn=1
-  } else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==true){
-    turn=2
-  } else if (turn==2 && players[0][1]==false && players[1][1]==false){
-    turn=2
-  } else {
-    turn=-1
-  }
-  turnHtml.innerText="player "+(turn+1).toString()+"'s turn to play";
-}
-
-function checkHold(turn){
-  if (players[turn][0]%10 == ((players[turn-1][0]%10)-1)){
-    return true;
-  }
-  else{
-    return false;
-  }
-}
-
-function checkPass(){
-  winBool=false
-  if (players[turn][0]%10==9){
-    squares[players[turn][0]].classList.remove('player'+(turn+1).toString());
-    statusEl.innerText="player "+(turn+1).toString()+" win";
-    layers[turn][2]=true;
-    winBool = checkWin();
-  }
-  return winBool
-}
-
-function hideButtons(){
-  moveUpBtn.style.visibility = 'hidden'
-  moveDownBtn.style.visibility = 'hidden'
-  holdBtn.style.visibility = 'hidden'
-  pushBtn.style.visibility = 'hidden'
 }
 
 function showButtons(){
@@ -222,6 +108,13 @@ function showButtons(){
     moveDownBtn.style.visibility = 'visible'
     holdBtn.style.visibility = 'visible'
   }
+}
+
+function hideButtons(){
+  moveUpBtn.style.visibility = 'hidden'
+  moveDownBtn.style.visibility = 'hidden'
+  holdBtn.style.visibility = 'hidden'
+  pushBtn.style.visibility = 'hidden'
 }
 
 function moveUp(){
@@ -305,6 +198,124 @@ function push(){
       }
   }
 }
+
+function checkDie(){
+  loseBool = false
+  if (players[turn][0]>=1 && players[turn][0]<=8){
+    if (tiles[(players[turn][0]%10)-1]==0){
+      statusEl.innerText="player "+(turn+1).toString()+" lost"
+      players[turn][1]=false
+      
+      loseBool=checkLose()
+    }
+  }
+  else if(players[turn][0]>=21 && players[turn][0]<=28){
+    if (tiles[(players[turn][0]%10)-1]==1){
+      statusEl.innerText="player "+(turn+1).toString()+" lost"
+      players[turn][1]=false
+      loseBool=checkLose()
+    }
+  }
+  return loseBool
+}
+// lose boolean is a boolean to state whether you lost or not the game, which you get it from the checklose function that you call it from the check die function
+
+function checkLose(){
+  for(let i = 0; i < 3; i++) {
+    if (players[i][1]==true){
+      return false
+    }
+  }
+  statusEl.innerText="you lose"
+  clearInterval(timer);
+  hideButtons()
+  return true
+}
+
+function checkPass(){
+  winBool=false
+  if (players[turn][0]%10==9){
+    squares[players[turn][0]].classList.remove('player'+(turn+1).toString());
+    statusEl.innerText="player "+(turn+1).toString()+" win";
+    players[turn][2]=true;
+    winBool = checkWin();
+  }
+  return winBool
+}
+
+function checkWin(){
+  win = false
+  for(let i = 0; i < 3; i++) {
+    if (players[i][2]==false && players[i][1]==true){
+      win = true
+    }
+  } 
+  if (win){
+    statusEl.innerText="you win"
+    clearInterval(timer);
+    hideButtons()
+  }
+  return win
+}
+
+
+
+function turns(){
+  if (turn==0 && players[0][0]%10==0){
+    turn=0
+  } else if (turn==0 && players[1][1]==true){
+    turn=1
+  } else if (turn==0 && players[1][1]==false && players[2][1]==false){
+    turn=0
+  } else if (turn==0 && players[1][1]==false && players[2][1]==true){
+    turn=2
+  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==false){
+    turn=0
+  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==true && players[0][2]==true){
+    turn=1
+  } else if (turn==1 && players[0][1]==false && players[2][1]==false){
+    turn=1
+  } else if (turn==1 && players[turn][0]%10==0 && players[0][1]==false){
+    turn=1
+  } else if (turn==1 && players[2][1]==true){
+    turn=2
+  } else if (turn==1 && players[2][1]==false && players[0][1]==true){
+    turn=0
+  } else if (turn==2 && players[0][1]==true && players[0][2]==false){
+    turn=0
+  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==false){
+    turn=1
+  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==true && players[1][2]==true){
+    turn=2
+  } else if (turn==2 && players[0][1]==true && players[0][2]==true && players[1][1]==false){
+    turn=2
+  } else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==false){
+    turn=1
+  } else if (turn==2 && players[0][1]==false && players[1][1]==true && players[1][2]==true){
+    turn=2
+  } else if (turn==2 && players[0][1]==false && players[1][1]==false){
+    turn=2
+  } else {
+    turn=-1
+  }
+  turnHtml.innerText="player "+(turn+1).toString()+"'s turn to play";
+}
+
+function checkHold(turn){
+  if (players[turn][0]%10 == ((players[turn-1][0]%10)-1)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+
+
+
+
+
 
 function toggleLightDark() {
   body.className = body.className === "dark" ? "" : "dark"
